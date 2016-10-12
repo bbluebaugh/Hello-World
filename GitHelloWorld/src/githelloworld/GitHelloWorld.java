0e51751eb0189441775103b648c98459bd23ca00
bwbluebaugh0 <bwbluebaugh0@CC270-86273.frostburg.edu>
2016-10-12 09:49:51 -0400

    actual thing

--- GitHelloWorld/src/githelloworld/GitHelloWorld.java
+++ GitHelloWorld/src/githelloworld/GitHelloWorld.java
@@ -4,18 +4,38 @@
  * and open the template in the editor.
  */
 package githelloworld;
+import java.io.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author bwbluebaugh0
  */
 public class GitHelloWorld {
-
+    public void GitHelloWorld(){
+    } 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        ArrayList<String> lines = new ArrayList<String>();
+        try{
+            
+            FileReader F = new FileReader(args[0]);
+            BufferedReader BR = new BufferedReader(F);
+            String line = "";
+            while((line = BR.readLine()) != null);
+            {
+                lines.add(line);
+            }
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("File not Found");
+        }
+        catch(IOException ex){
+            System.out.println("Input not great");
+        }
     }
     
 }
